fdf99f0621082d15b19a25d7265f55973f2e1e7c
Jan de Leeuw <jan@deleeuwpdx.net>
2025-10-02 15:18:53 -0400

    e25

--- public/js/wowchemy-slides.js
+++ public/js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = {highlight_style: "dracula", theme: "black"};
 
-  // ns-hugo:/Users/deleeuw/Library/Mobile Documents/com~apple~CloudDocs/jansweb/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/assets/js/wowchemy-utils.js
+  // ns-hugo:/Users/deleeuw/jansweb/themes/github.com/wowchemy/wowchemy-hugo-modules/wowchemy/assets/js/wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
